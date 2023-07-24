c6e942529edf179572e42176c00af2474b5e5e24
NataliaStekolnikova <natalia.a.stekolnikova@gmail.com>
2023-07-24 22:31:23 +0300

    user registration has been implemented

--- feedback_analyzer/users/views.py
+++ feedback_analyzer/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Users
 
-# Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if email and password:
+            user = Users.objects.create_user(email=email, password=password)
+            # Default validators: UserAttributeSimilarityValidator, MinimumLengthValidator, CommonPasswordValidator, NumericPasswordValidator
+            # Завалидировать емэйл, совпадаел ли эмэйл!!! https://www.abstractapi.com/guides/django-email-validation
+            # Закоментировано, пока не сделана форма login
+            # return redirect('login')
+    return render(request, 'registration.html')
